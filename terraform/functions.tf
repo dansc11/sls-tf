1015b1c87457fd22bb2a6f879847cb78b0b7918d
Daniel Scott <danscott123@gmail.com>
2021-12-06 17:28:10 +0000

    Set function_name

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -1,9 +1,9 @@
 resource "aws_lambda_function" "function" {
   for_each = { for function in local.functions : function.name => function }
 
-  name     = each.key
-  filename = each.value.filename
-  role     = each.value.role
-  handler  = each.value.handler
-  runtime  = each.value.runtime
+  function_name = each.key
+  filename      = each.value.filename
+  role          = each.value.role
+  handler       = each.value.handler
+  runtime       = each.value.runtime
 }
